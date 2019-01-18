2571cc6f32c12ab7e400651199491ae1005319d9
Anish Kumar Yadav <40102074+anish-yadav@users.noreply.github.com>
2019-01-19 01:08:46 +0530

    Update images.py

--- images.py
+++ images.py
@@ -4,10 +4,11 @@
 # 2. pip install BeautifulSoup
 
 import sys
+import pymongo
 import requests
 from PIL import Image
 from io import BytesIO
-from bs4 import BeautifulSoup 
+from bs4 import BeautifulSoup
 
 
 if len(sys.argv) == 1:
@@ -15,11 +16,11 @@ if len(sys.argv) == 1:
 else:
     search =  " ".join(sys.argv[1:])
 
-url = "https://www.bing.com/images/search?q={}".format(search)
+url = "https://in.images.search.yahoo.com/search/images;?p={}".format(search)
 r = requests.get(url)
 soup = BeautifulSoup(r.text,"html.parser")
-aTag = soup.findAll("a",{"class":"thumb"})
-result  = requests.get(aTag[1].attrs["href"])
+images_array = soup.find_all("img",{"class":"process"})
+result  = requests.get(images_array[0].attrs["data-src"])
 try :
    img = Image.open(BytesIO(result.content))
    img.save("{}.{}".format(search,img.format))
